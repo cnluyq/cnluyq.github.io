1fed5c4f1a908d4a43acc49807a4e6b365d17024
yql <yqmailpub@163.com>
2025-08-11 11:10:05 +0800

    update

--- problem_manager/problems/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Problem
-
-@admin.register(Problem)
-class ProblemAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'key_words', 'created_by', 'create_time')
-    search_fields = ('title', 'key_words', 'description')
